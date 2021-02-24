b86cd1e8f3ae02f8c72140dd42356be5b099690c
Jessica Whitman <oprah135@gmail.com>
2021-02-24 11:22:08 -0800

    added handleStateChange and handleSubmit functions

--- src/components/EmployeeContain.js
+++ src/components/EmployeeContain.js
@@ -3,28 +3,54 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import retrieveUsers from "./utils/RandomUserAPI";
 import Container from "react-bootstrap/Container";
+import EmployeeList from "./EmployeeList";
 class EmployeeMain extends Component {
   constructor() {
     super();
     this.state = {
       search: "",
       employees: [],
-      isSorted: true,
+      sort: true,
     };
     this.handleStateChange = this.handleStateChange.bind(this);
   }
   handleStateChange(e) {
     this.setState({ search: e.target.value });
   }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      search: "",
+    });
+  }
+  handleSort() {
+    this.state.sort === true
+      ? this.setState({ sort: false })
+      : this.setState({ sort: true });
+  }
+  render() {
+    let filterByName = this.state.results.filter((employee) => {});
+
+    let employeeSort;
+
+    if (this.state.search === "") {
+      employeeSort = this.state.employees;
+    } else {
+      employeeSort = filterByName;
+    }
+
+    return (
+      <>
+        <Container fluid>
+          <Row>
+            <Col>
+              <EmployeeList />
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
 }
 
-return (
-  <>
-    <Container fluid>
-      <Row>
-        <Col></Col>
-      </Row>
-    </Container>
-  </>
-);
 export default EmployeeMain;
