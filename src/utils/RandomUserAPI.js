0a2f29674ed9ec584c20e0b758b1cefd16862b6b
Jessica Whitman <oprah135@gmail.com>
2021-02-25 13:23:47 -0800

    preparing to deploy

--- src/utils/RandomUserAPI.js
+++ src/utils/RandomUserAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // Retrieve an array of random users
-const URL = "https://randomuser.me/api/?results=100";
+const URL = "https://randomuser.me/api/?results=100&nat=us,dk,fr,gb";
 function retrieveUsers() {
   return axios.get(URL);
 }
